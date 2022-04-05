84f39d98724a1c9e6895eec7741bb09192accb23
BertoPolo <bertopolo91@gmail.com>
2022-04-05 14:52:40 +0200

    server finished , lets g0 with index.js

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import listEndpoints from "express-list-endpoints"
+import authorsRouter from " ./services/authors.index.js"
+
+const server = express()
+
+const port = 3001
+
+server.use(express.json())
+
+//endpoints
+
+server.use("/authors", authorsRouter)
+
+server.listen(port, () => {
+  console.table(listEndpoints(server))
+  console.log("server run on this port : ", port)
+})
---
+++ src/services/authors/index.js
@@ -0,0 +1,4 @@
+import express from "express"
+import fs from "fs"
+import { fileURLToPath } from "url"
+import { dirname, join } from "path"
