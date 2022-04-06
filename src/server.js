0011b3381bb16e9cac6b6c3ba2767b2458aaf9e3
BertoPolo <bertopolo91@gmail.com>
2022-04-06 17:13:35 +0200

    .

--- src/server.js
+++ src/server.js
@@ -2,17 +2,32 @@ import express from "express"
 import listEndpoints from "express-list-endpoints"
 import authorsRouter from "./services/authors/index.js"
 import blogsRouter from "./services/blogs/index.js"
+import { genericErrorHandler, notFoundErrorHandler, badRequestErrorHandler, unauthorizedErrorHandler } from "./errorHandlers.js"
 
 const server = express()
 
 const port = 3001
 
+//login middleware
+const loggerMiddleware = (req, res, next) => {
+  console.log(`Request method: ${req.method} --- URL ${req.url} --- ${new Date()}`)
+  // req.name = "Alberto"
+  next()
+}
+
+server.use(loggerMiddleware) // login
 server.use(express.json())
 
 //endpoints
 
 server.use("/authors", authorsRouter)
 server.use("/blogs", blogsRouter)
+
+//ERROR MIDDLEWARES
+server.use(badRequestErrorHandler) // 400
+server.use(unauthorizedErrorHandler) // 401
+server.use(notFoundErrorHandler) // 404
+server.use(genericErrorHandler) // 500
 
 server.listen(port, () => {
   console.table(listEndpoints(server))
--- src/services/blogs/validation.js
+++ src/services/blogs/validation.js
@@ -1,7 +1,7 @@
 import { checkSchema, validationResult } from "express-validator"
 import createError from "http-errors"
 
-const blogsSchema = () => {
+/* const blogsSchema = () => {
   category: "ARTICLE CATEGORY",
   title: "ARTICLE TITLE",
   cover: "ARTICLE COVER (IMAGE LINK)",
@@ -15,18 +15,15 @@ const blogsSchema = () => {
   },
   content: "HTML",
   createdAt: "NEW DATE"
-}
+} */
 
 export const checkBookSchema = checkSchema(blogsSchema)
 
-
 export const checkValidationResult = (req, res, next) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-    
-      next(createError(400, "Validation problems in req.body", { errorsList: errors.array() }))
-    } else {
-      
-      next()
-    }
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    next(createError(400, "Validation problems in req.body", { errorsList: errors.array() }))
+  } else {
+    next()
   }
+}
