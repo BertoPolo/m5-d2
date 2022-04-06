14a8b51cb551b882e88b438685592f1541b4ff6c
BertoPolo <bertopolo91@gmail.com>
2022-04-06 15:31:45 +0200

    until 1st get

--- src/services/blogs/index.js
+++ src/services/blogs/index.js
@@ -8,6 +8,34 @@ const blogsRouter = express.Router()
 
 const authorsJSONPath = join(dirname(fileURLToPath(import.meta.url)), "blogs.json")
 
-blogsRouter.post("/", (req, res) => {})
+const readBlogs = () => JSON.parse(fs.readFileSync(authorsJSONPath))
+
+const writeBlog = (content) => fs.writeFileSync(authorsJSONPath, JSON.stringify(content))
+
+blogsRouter.post("/", (req, res, next) => {
+  try {
+    const newBlog = { ...req.body, createdAt: new Date(), id: uniqid() }
+    const blogs = readBlogs().push(newBlog)
+
+    res.status(201).send({ id: newBlog.id })
+  } catch (error) {
+    // next(error)
+  }
+})
+
+blogsRouter.get("/", (req, res, next) => {
+  try {
+    const blogs = readBlogs()
+
+    if (req.query && req.query.category) {
+      const filteredBlogs = blogs.filter((blog) => blog.category === req.query.category)
+      res.send(filteredBlogs)
+    } else {
+      res.send(blogs)
+    }
+  } catch (error) {
+    // next(error)
+  }
+})
 
 export default blogsRouter
